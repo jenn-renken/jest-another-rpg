c675bddc715b64889fb85c3928db21598f698a5e
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-10-19 16:59:10 -0400

    added player objects and tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
@@ -12,4 +13,23 @@ test('creates a player object', () => {
   expect(player.agility).toEqual(expect.any(Number));
 
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
